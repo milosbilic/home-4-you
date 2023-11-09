ed3e7b2a28d020ce667e6c0d3911b8fd72167ab5
Milos Bilic <milosrcz@gmail.com>
2023-11-09 22:30:32 +0100

    Bum java version to 21

--- src/main/java/home/four/you/exception/ResourceNotFoundException.java
+++ src/main/java/home/four/you/exception/ResourceNotFoundException.java
@@ -3,6 +3,8 @@ package home.four.you.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 import static home.four.you.exception.ErrorCode.NOT_FOUND;
 
 /**
@@ -11,6 +13,7 @@ import static home.four.you.exception.ErrorCode.NOT_FOUND;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends Home4YouException {
 
+	@Serial
 	private static final long serialVersionUID = 3346530278343930771L;
 
 	public ResourceNotFoundException() {
